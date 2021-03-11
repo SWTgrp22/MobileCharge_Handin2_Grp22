a27b20835edb44004f30f6d19965a172f468dc15
Ditte Bertelsen <201907172@uni.au.dk>
2021-03-11 13:13:44 +0100

    Add project files.

---
+++ MobileCharge_Handin2_Grp22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MobileCharge_Handin2_Grp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
