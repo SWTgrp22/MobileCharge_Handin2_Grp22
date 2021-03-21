96db17397060016db641bfaabc2d86122988ee04
FrejaIBredahl <201904700@post.au.dk>
2021-03-21 13:02:50 +0100

    Oprettet en TestDisplay-klasse

---
+++ CharginMonitor.Test.Unit/TestDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace ChargingMonitor.Test.Unit
+{
+    public class TestDisplay
+    {
+        private Display _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new Display();
+
+        }
+    }
+}
