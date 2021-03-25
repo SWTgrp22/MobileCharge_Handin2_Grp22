fb96113f80daa15ebe93ec4b217b8d04865ea3d3
FrejaIBredahl <201904700@post.au.dk>
2021-03-25 14:01:04 +0100

    Tilføjelse af tests af RFID

---
+++ CharginMonitor.Test.Unit/RFID testklasser/TestStationControl_RfidReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChargingMonitor.RFIDReader;
+using Ladeskab;
+using NSubstitute;
+using NUnit.Framework;
+using UsbSimulator;
+
+namespace ChargingMonitor.Test.Unit.RFID_testklasser
+{
+    public class TestStationControl_RfidReader
+    {
+        private StationControl _uut;
+        private IRFIDReader rfidReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            rfidReader = Substitute.For<IRFIDReader>();
+            _uut = new StationControl(new Door.Door(), new ChargeControl(new UsbChargerSimulator()), rfidReader,
+                new Display(), new LogFiles.Log());
+        }
+
+        [TestCase(0)]
+        [TestCase(210)]
+        [TestCase(555555)]
+        [TestCase(2147483647)]
+        public void RfidDetected_DetectedTag_IDIsEqualToID(int newId)
+        {
+            //Act
+            rfidReader.RFIDReaderEvent += Raise.EventWith(new RFIDReaderEventArg { ID = newId });
+
+            //Assert
+            Assert.That(_uut._rfidID, Is.EqualTo(newId));
+
+        }
+
+      
+    }
+}
