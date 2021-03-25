94a930e9e4528cf6d36efc13c7d888467a885201
Ditte Bertelsen <201907172@uni.au.dk>
2021-03-25 12:19:44 +0100

    Tilføjelse af testklasse til stationcontrol_Door

---
+++ CharginMonitor.Test.Unit/Door testklasser/TestStationControl_Door.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChargingMonitor.Door;
+using ChargingMonitor.RFIDReader;
+using Ladeskab;
+using NSubstitute;
+using NUnit.Framework;
+using UsbSimulator;
+
+namespace ChargingMonitor.Test.Unit
+{
+    public class TestStationControl_Door
+    {
+        private StationControl _uut;
+        private IDoor door;
+
+        [SetUp]
+        public void Setup()
+        {
+            door = Substitute.For<IDoor>();
+            _uut = new StationControl(door, new ChargeControl(new UsbChargerSimulator()), new RfidReader(),
+                new Display(), new Log());
+        }
+
+    }
+}
